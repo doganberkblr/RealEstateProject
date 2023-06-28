ed4d063e672f6400cd344b510ee1f7f88ddc38d0
Dogan Berk BULUR <116250125+doganberkblr@users.noreply.github.com>
2023-06-29 02:32:46 +0300

    29.03.2023-2

--- RealEstateProject/Controllers/MusteriMesajController.cs
+++ RealEstateProject/Controllers/MusteriMesajController.cs
@@ -10,6 +10,46 @@ namespace RealEstateProject.Controllers
     public class MusteriMesajController : Controller
     {
         MusteriMesajManager kt=new MusteriMesajManager(new EFMusteriMesajDAL());
+		public IActionResult MusteriMesajGonder()
+		{
+			return View();
+			
+		}
+		[HttpPost]
+		public IActionResult MusteriMesajGonder(MusteriMesaj musteriMesaj)
+		{
+			try
+			{
+				kt.Tadd(musteriMesaj);
+				return RedirectToAction("LoginIndex", "Default");
+
+
+			}
+			catch (Exception ex)
+			{
+				return RedirectToAction("HataSayfasi", "Hata");
+			}
+		}
+        public IActionResult KullaniciMesajGonder()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult KullaniciMesajGonder(MusteriMesaj musteriMesaj)
+        {
+            try
+            {
+                kt.Tadd(musteriMesaj);
+                return RedirectToAction("Index", "Default");
+
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("HataSayfasi", "Hata");
+            }
+        }
         public IActionResult AdminMusteriMesajListeleme(int sayfa=1)
         {
             var musteriMesajlari = kt.TgetList().ToPagedList(sayfa, 3);
